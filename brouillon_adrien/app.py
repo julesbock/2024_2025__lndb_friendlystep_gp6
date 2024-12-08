f40b8853a9d483f55e3b3e476981bade48d5323a
adrilnd <barouxarbonaadrien@gmail.com>
2024-12-08 23:44:34 +0100

    d

--- brouillon_adrien/app.py
+++ brouillon_adrien/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 # import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
-def welcome():
+def index():
     return render_template("index.html")
 
 # @app.route("/heure")
@@ -35,6 +35,20 @@ def welcome():
 def login():
     return render_template("login.html")
 
+@app.route('/traitement', methods=["POST", "GET"])
+def traitement():
+    if request.method == "POST":
+        # print(request.args)
+        donnees = request.form
+        nom = donnees.get('nom')
+        mdp = donnees.get('mdp')
+        if nom == 'admin' and mdp == "1234":
+            return render_template("traitement.html", nom_utilisateur = nom)
+        else:
+            return render_template("traitement.html")
+        # return "Traitement de données" render_template("traitement.html")
+    else:
+        return redirect(url_for('index'))
 if __name__ =='__main__':
     app.run(debug=True)
 
