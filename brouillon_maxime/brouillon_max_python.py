fd444f13789ab9b73b64e943ca134e4e2726e58a
MaxBOURG-Sav <maxbourg74@gmail.com>
2024-12-09 10:58:27 +0100

    Update brouillon_max_python.py

--- brouillon_maxime/brouillon_max_python.py
+++ brouillon_maxime/brouillon_max_python.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from data_brouillon.py import *
 app = Flask(__name__)
 app.secret_key = "039291df98be34655173c930564e88b1098ac6e1a173a61eacd268b6af8d7c44"
 
@@ -37,6 +36,11 @@ def submit():
 if __name__ == '__main__':
     app.run(debug=True)
 
+def recherche_utilisateur(nom_utilisateur, mot_de_passe):
+    for utilisateur in utilisateurs:
+        if utilisateur ['nom'] == nom_utilisateur and utilisateur['mdp'] == mot_de_passe:
+            return utilisateur
+    return None
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -44,14 +48,25 @@ def login():
         donnees=request.form
         nom= donnees.get("nom")
         mdp= donnees.get("mdp")
-        if nom =="Maxime" and mdp== "2468":
+
+        utilisateur= recherche_utilisateur(nom, mdp)
+        if utilisateur is not None:
+            print("utilisateur trouvé")
+            session['nom_utilisateur'] = utilisateur['nom']
+            print(session)
             return redirect(url_for('index'))
         else:
+            print("utilisateur inconnu")
             return redirect(request.url)
     else:
+        if 'nom_utilisateur' in session:
+            return  redirect(url_for('index'))
         return render_template("login.html")
 
-
+utilisateurs= [{"nom":"maxime", "mdp": "123456"},
+              {"nom": "jules", "mdp" : "2008"},
+              {"nom": "adrien", "mdp": "3546879"}
+              ]
 
 @app.route ("/compteur")
 def compteur():
