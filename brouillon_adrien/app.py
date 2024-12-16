e5beb66babbb1756edcaf693dedbb7de87726605
adrilnd <barouxarbonaadrien@gmail.com>
2024-12-16 23:16:32 +0100

    la fonction create_user marche
    faut voir juste si on fait un fichier qui repertorie tous les noms des utilisateurs afin de savoir quand la personne crée son compte si le nom d'utilisateur est deja pris car il ne peux y avoir 2 fois comptes avec le meme username

--- brouillon_adrien/app.py
+++ brouillon_adrien/app.py
@@ -1,18 +1,125 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import datetime
-import os
+import os, json
 
 app = Flask(__name__)
 app.secret_key = "02458d8b8e67adc5966be5f1b3e109341e2507abfd007fa2785cde99996611e5"
+
 @app.route('/')
 def homepage():
     return render_template("homepage.html")
+
 
 users = [
     {"nom" : 'admin', "mdp":"1234"},
     {"nom" : 'marie', "mdp":"nsi"},
     {"nom" : 'paul', "mdp":"azerty"}
 ]
+
+
+def recherch_user(name_user, mdp):
+    for user in users:
+        if user["nom"] == name_user and user["mdp"] == mdp:
+            return user
+    return None
+
+
+def create_user(user_personnal_data): #user_personal_data est la récupération des données et doit être un dictionnaire afin de placer dans le fichier json le dico
+    name = user_personnal_data[0]  # Récupère le nom de l'utilisateur
+    dossier_projet = os.path.dirname(__file__)
+    # Chemin du dossier 'users'
+    chemin_users = os.path.join(dossier_projet, "users", name)
+    print(chemin_users)
+    # Crée le dossier s'il n'existe pas déjà
+    if not os.path.exists(chemin_users):
+        os.mkdir(chemin_users)
+        first_file_name = "personnal_data"
+        second_file_name = "user_data"
+        chemin_first_file = os.path.join(chemin_users, first_file_name + ".json")
+        chemin_second_file = os.path.join(chemin_users, second_file_name + ".json")
+        with open(chemin_first_file, "w") as json_file:
+            json.dump(user_personnal_data, json_file, indent=4)
+        with open(chemin_second_file, "w") as json_file:
+            json.dump(user_personnal_data, json_file, indent=4)
+        print(f"Dossier pour {name} créé à : {chemin_users}")
+    else:
+        print(f"Le dossier pour {name} existe déjà.")
+    
+
+
+@app.route('/login',  methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        # print(request.args)
+        donnees = request.form
+        nom = donnees.get('nom')
+        mdp = donnees.get('mdp')
+        user=recherch_user(nom, mdp)
+        if user is not None:
+            print('utilisateur trouvé')
+            session["name_user"] = user['nom']
+            print(session)
+            return redirect(url_for('homepage'))
+        else:
+            print('utilisateur inconnu')
+            d=["Maxime"]
+            create_user(d)
+            print(d)
+            session["name_user"] = d[0]
+            print(session)
+            return redirect(url_for('homepage'))
+        # return "Traitement de données" render_template("traitement.html")
+    else:
+        print(session)
+        if "name_user" in session:
+            return redirect(url_for('homepage'))
+        return render_template("login.html")
+    
+
+@app.route('/logout')
+def logout():
+    print(session)
+    session.pop("name_user", None)
+    print(session)
+    return redirect(url_for('login'))
+
+
+@app.route('/logout_confirmation')
+def logout_confirmation():
+    return render_template("logout_confirmation.html")
+
+
+@app.route('/formulaire')
+def formulaire():
+    return render_template("formulaire.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @app.route("/heure")
@@ -39,66 +146,6 @@ def heure():
 #     else:
 #         eleves_selectionnes = []
 #     return render_template("eleves.html", eleves=eleves_selectionnes)
-
-def recherch_user(name_user, mdp):
-    for user in users:
-        if user["nom"] == name_user and user["mdp"] == mdp:
-            return user
-    return None
-def create_user(user_personnal_data):
-    name = user_personnal_data[0]  # Récupère le nom de l'utilisateur
-    user_folder_path = os.path.join("users" + str(name))  # Crée le chemin relatif pour le dossier utilisateur
-    
-    # Crée le dossier s'il n'existe pas déjà
-    if not os.path.exists(user_folder_path):
-        os.mkdir(user_folder_path)
-        print(f"Dossier pour {name} créé à : {user_folder_path}")
-    else:
-        print(f"Le dossier pour {name} existe déjà.")
-    
-
-@app.route('/login',  methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        # print(request.args)
-        donnees = request.form
-        nom = donnees.get('nom')
-        mdp = donnees.get('mdp')
-        user=recherch_user(nom, mdp)
-        if user is not None:
-            print('utilisateur trouvé')
-            session["name_user"] = user['nom']
-            print(session)
-            return redirect(url_for('homepage'))
-        else:
-            print('utilisateur inconnu')
-            d=["Sophie"]
-            create_user(d)
-            print(d)
-            session["name_user"] = d[0]
-            print(session)
-            return redirect(url_for('homepage'))
-        # return "Traitement de données" render_template("traitement.html")
-    else:
-        print(session)
-        if "name_user" in session:
-            return redirect(url_for('homepage'))
-        return render_template("login.html")
-    
-@app.route('/logout')
-def logout():
-    print(session)
-    session.pop("name_user", None)
-    print(session)
-    return redirect(url_for('login'))
-
-@app.route('/logout_confirmation')
-def logout_confirmation():
-    return render_template("logout_confirmation.html")
-
-@app.route('/formulaire')
-def formulaire():
-    return render_template("formulaire.html")
 
 @app.route('/compteur')
 def compteur():
