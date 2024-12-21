de285247a9395853e42ca0174d2dc3cc6e3858e7
adrilnd <barouxarbonaadrien@gmail.com>
2024-12-21 02:25:16 +0100

    testing user creation

--- brouillon_adrien/app.py
+++ brouillon_adrien/app.py
@@ -18,34 +18,56 @@ users = [
 
 
 def recherch_user(name_user, mdp):
-    for user in users:
-        if user["nom"] == name_user and user["mdp"] == mdp:
+    dossier_projet = os.path.dirname(__file__)
+    chemin_users_list = os.path.join(dossier_projet, "users", "users_list.json")
+    with open(chemin_users_list, "r") as json_file:
+            all_data = json.load(json_file)
+    for user in all_data["users_list"]:
+        if user["username"] == name_user and user["mdp"] == mdp:
             return user
     return None
 
 
-def create_user(user_personnal_data): #user_personal_data est la récupération des données et doit être un dictionnaire afin de placer dans le fichier json le dico
-    name = user_personnal_data["name"]  # Récupère le nom de l'utilisateur
+def create_and_register_user(user_personnal_data): #user_personal_data est la récupération des données et doit être un dictionnaire afin de placer dans le fichier json le dico
+    name = user_personnal_data["username"]  # Récupère le nom de l'utilisateur
     dossier_projet = os.path.dirname(__file__)
     # Chemin du dossier 'users'
     chemin_users = os.path.join(dossier_projet, "users", name)
-    print(chemin_users)
+    # print(chemin_users)
     # Crée le dossier s'il n'existe pas déjà
     if not os.path.exists(chemin_users):
-        os.mkdir(chemin_users)
-        first_file_name = "personnal_data"
-        second_file_name = "user_data"
-        chemin_first_file = os.path.join(chemin_users, first_file_name + ".json")
-        chemin_second_file = os.path.join(chemin_users, second_file_name + ".json")
-        with open(chemin_first_file, "w") as json_file:
-            json.dump(user_personnal_data, json_file, indent=4)
-        with open(chemin_second_file, "w") as json_file:
-            json.dump(user_personnal_data, json_file, indent=4)
-        print(f"Dossier pour {name} créé à : {chemin_users}")
+        create_user(user_personnal_data, chemin_users)
+        register_user(name, user_personnal_data['mdp'], dossier_projet)
+        # print(f"Dossier pour {name} créé à : {chemin_users}")
     else:
-        print(f"Le dossier pour {name} existe déjà.")
-    
+        # print(f"Le dossier pour {name} existe déjà.")
+        pass
+    return redirect(url_for("homepage"))
 
+def create_user(user_personnal_data, chemin_users):
+    os.mkdir(chemin_users)
+    first_file_name = "personnal_data"
+    second_file_name = "user_data"
+    chemin_first_file = os.path.join(chemin_users, first_file_name + ".json")
+    chemin_second_file = os.path.join(chemin_users, second_file_name + ".json")
+    with open(chemin_first_file, "w") as json_file:
+        json.dump(user_personnal_data, json_file, indent=4)
+    with open(chemin_second_file, "w") as json_file:
+        json.dump(None, json_file, indent=4)
+        
+        # print(f"Dossier pour {name} créé à : {chemin_users}")
+
+def register_user(username, mdp, chemin_projet):
+    chemin_users_list = os.path.join(chemin_projet, "users", "users_list.json")
+    user_and_mdp = {
+        "username" : username,
+        "mdp" : mdp
+    }
+    with open(chemin_users_list, "r") as json_file:
+        all_data = json.load(json_file)
+    all_data ["users_list"].append(user_and_mdp)
+    with open(chemin_users_list, "w") as json_file:
+        json.dump(all_data, json_file, indent=4)
 
 @app.route('/login',  methods=["POST", "GET"])
 def login():
@@ -57,7 +79,7 @@ def login():
         user=recherch_user(nom, mdp)
         if user is not None:
             print('utilisateur trouvé')
-            session["name_user"] = user['nom']
+            session["name_user"] = user['username']
             print(session)
             return redirect(url_for('homepage'))
         else:
@@ -74,25 +96,42 @@ def login():
 def sign_up():
     if request.method == "POST":
         # Récupération des données du formulaire
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        username = request.form['username']
-        email = request.form['email']
-        birth_day = request.form['birth_day']
-        birth_month = request.form['birth_month']
-        birth_year = request.form['birth_year']
-        gender_choice = request.form['gender_choice']
-        origin = request.form['origin']
-        height = request.form['height']
-        weight = request.form['weight']
-        print(first_name, last_name, username,email, birth_day, birth_month, birth_year,
-              gender_choice, origin,height,weight)
-
+        # first_name = request.form['first_name']
+        # last_name = request.form['last_name']
+        # username = request.form['username']
+        # email = request.form['email']
+        # birth_day = request.form['birth_day']
+        # birth_month = request.form['birth_month']
+        # birth_year = request.form['birth_year']
+        # gender_choice = request.form['gender_choice']
+        # origin = request.form['origin']
+        # height = request.form['height']
+        # weight = request.form['weight']
+        # print(first_name, last_name, username,email, birth_day, birth_month, birth_year,
+        #       gender_choice, origin,height,weight)
+        user_dico = {
+            "first_name" : request.form['first_name'],
+            "last_name" : request.form['last_name'],
+            "username" : request.form['username'],
+            "mdp" : request.form['mdp'],
+            "email" : request.form['email'],
+            "birth_day" : request.form['birth_day'],
+            "birth_month" : request.form['birth_month'],
+            "birth_year" : request.form['birth_year'],
+            "gender_choice" : request.form['gender_choice'],
+            "origin" : request.form['origin'],
+            "height" : request.form['height'],
+            "weight" : request.form['weight']
+        }
+        create_and_register_user(user_dico)
         # Traite ici les données reçues, par exemple, en les enregistrant dans une base de données
-        print(f"Prénom: {first_name}, Nom: {last_name}, Email: {email}")
+        # print(f"Prénom: {first_name}, Nom: {last_name}, Email: {email}")
+        session["name_user"] = user_dico['username']
 
-
+        return redirect(url_for('homepage'))
+    
     return render_template("sign_up.html")
+
 @app.route('/logout')
 def logout():
     print(session)
