5470d32487dc8d8e7db71184c1378039052e0413
julesbock <julesbock08@icloud.com>
2025-01-31 17:18:56 +0100

    c
    Update :
    New dedicated blueprint for "/profile"

--- main/app.py
+++ main/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from datetime import date
 from tools import *
 from app_routes.sign_up_ap import *
+from app_routes.profile_ap import * 
 from app_routes.login_ap import *
 from app_routes.logout_ap import *
 from app_routes.tournament_ap import *
@@ -30,7 +31,6 @@ def user_data_graphics(data_type, label_name):
     else:
         return render_template("user_data_graphics.html", label=label_name)
     
-
 @app.route('/data_input', methods=["POST", "GET"])
 def data_input ():
     date_time = datetime.datetime.now()
@@ -60,34 +60,12 @@ def data_input ():
         return redirect(url_for('root'))
     else : 
         return render_template("data_input.html", today_date = today_date, data = existing_data)
-    
-@app.route("/profil", methods=["POST", "GET"])
-def profile():
-    if request.method == "POST":
-        user_personnal_data = {
-            "first_name": request.form.get('first_name'),
-            "last_name": request.form.get('last_name'),
-            "username": request.form.get('username'),
-            "mdp": request.form.get('mdp'),
-            "email": request.form.get('email'),
-            "birth_day": request.form.get('birth_day'),
-            "birth_month": request.form.get('birth_month'),
-            "birth_year": request.form.get('birth_year'),
-            "gender_choice": request.form.get('gender_choice'),
-            "nationality": request.form.get('nationality'),
-            "height": request.form.get('height'),
-            "weight": request.form.get('weight')
-        }
-        give_data("personnal_data.json", user_personnal_data)
-        return redirect(url_for('profile'))
-    else:
-        user_personnal_data = get_data("personnal_data.json")
-        return render_template("profil_settings.html", user=user_personnal_data)
 
 app.register_blueprint(login_blueprint)
 app.register_blueprint(logout_blueprint)
 
-app.register_blueprint(sign_up_blueprint)  
+app.register_blueprint(sign_up_blueprint)
+app.register_blueprint(profil_blueprint)
 app.register_blueprint(tournaments_blueprint)
 
 @app.route("/faq")
@@ -102,5 +80,5 @@ for rule in app.url_map.iter_rules():
     print(rule)
 
 if __name__ =='__main__':
-    app.run(debug=True)
+    app.run(debug=True, port = 5001)
 
---
+++ main/app_routes/profile_ap.py
@@ -0,0 +1,27 @@
+from flask import Flask, Blueprint, render_template, request, redirect, url_for, session
+from tools import * 
+
+profil_blueprint = Blueprint("profil", __name__, url_prefix="/profil")
+
+@profil_blueprint.route("/", methods=["POST", "GET"])
+def profile():
+    if request.method == "POST":
+        user_personnal_data = {
+            "first_name": request.form.get('first_name'),
+            "last_name": request.form.get('last_name'),
+            "username": request.form.get('username'),
+            "mdp": request.form.get('mdp'),
+            "email": request.form.get('email'),
+            "birth_day": request.form.get('birth_day'),
+            "birth_month": request.form.get('birth_month'),
+            "birth_year": request.form.get('birth_year'),
+            "gender_choice": request.form.get('gender_choice'),
+            "nationality": request.form.get('nationality'),
+            "height": request.form.get('height'),
+            "weight": request.form.get('weight')
+        }
+        give_data("personnal_data.json", user_personnal_data)
+        return redirect(url_for('profile'))
+    else:
+        user_personnal_data = get_data("personnal_data.json")
+        return render_template("profil_settings.html", user=user_personnal_data)
